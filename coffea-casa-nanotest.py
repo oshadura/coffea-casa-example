1b5a250e6159bd0a41875c76e017854fb9dafb3a
Oksana Shadura <oksana.shadura@cern.ch>
2020-05-23 18:50:11 +0200

    Add TLS support for Dask + formating

--- coffea-casa-nanotest.py
+++ coffea-casa-nanotest.py
@@ -13,10 +13,7 @@ from coffea.analysis_objects import JaggedCandidateArray
 from coffea.processor.test_items import NanoTestProcessor
 from dask.distributed import Client, LocalCluster
 from dask_jobqueue import HTCondorCluster
-
-treename = 'Events'
-compression = 2
-
+from distributed.security import Security
 
 #'ZJets': ['/mnt/hadoop/user/uscms01/pnfs/unl.edu/data4/cms/store/user/oshadura/nano_dy.root'],
 #'Data' : ['/mnt/hadoop/user/uscms01/pnfs/unl.edu/data4/cms/store/user/oshadura/nano_dimuon.root']
@@ -24,15 +21,37 @@ filelist = {
         'ZJets': ['data/nano_dy.root'],
         'Data' : ['data/nano_dimuon.root']
         }
-
 treename = 'Events'
 compression = 2
 
+#Coffea test processor
 proc = NanoTestProcessor()
 
-cluster = HTCondorCluster(cores=4, memory="2GB", disk="1GB", log_directory="logs", silence_logs="debug", scheduler_options={"dashboard_address":"9998"}, job_extra={"universe": "docker", "docker_network_type": "host", "docker_image": "oshadura/coffea-casa:latest", "container_service_names": "condor", "condor_container_port": "8787", "should_transfer_files": "YES", "when_to_transfer_output": "ON_EXIT"})
+sec_dask = Security(tls_ca_file='/etc/cmsaf-secrets/ca.pem',
+               tls_worker_cert='/etc/cmsaf-secrets/usercert.pem',
+               tls_worker_key='/etc/cmsaf-secrets/userkey.pem',
+               tls_scheduler_cert='/etc/cmsaf-secrets/hostcert.pem',
+               tls_scheduler_key='/etc/cmsaf-secrets/hostkey.pem',
+               require_encryption=True)
+
+cluster = HTCondorCluster(cores=4,
+                          memory="2GB",
+                          disk="1GB",
+                          log_directory="logs",
+                          silence_logs="debug",
+                          scheduler_options={"dashboard_address":"9998"},
+                          # HTCondor submit script
+                          job_extra={"universe": "docker",
+                                     # Generated in coffea-casa:latest
+                                     "encrypt_input_files": "/etc/cmsaf-secrets/xcache_token",
+                                     "docker_network_type": "host",
+                                     "docker_image": "oshadura/coffea-casa-analysis:latest",
+                                     "container_service_names": "condor",
+                                     "condor_container_port": "8787",
+                                     "should_transfer_files": "YES",
+                                     "when_to_transfer_output": "ON_EXIT"})
 cluster.scale(jobs=1)
-client = Client(cluster)
+client = Client(cluster, security=sec_dask)
 
 print("Dask client: ", client)
 
